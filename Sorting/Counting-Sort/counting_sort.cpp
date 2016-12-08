f6e2b882fbd436b7681e7464d5d58000b5e0353b
Ashutosh Maurya <ashutoshmauryabgp@gmail.com>
2016-12-09 00:05:37 +0530

    Create counting_sort.cpp

---
+++ Sorting/Counting-Sort/counting_sort.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void counting_sort(int A[], int sortedA[], int N) {
+
+    // First, find the maximum value in A[]
+    int K = 0;
+    for(int i=0; i<N; i++) {
+        K = max(K, A[i]);
+    }
+	int Aux[K+1];
+    // Initialize the elements of Aux[] with 0
+    for(int i=0 ; i<=K; i++) {
+        Aux[i] = 0;
+    }
+
+    // Store the frequencies of each distinct element of A[],
+    // by mapping its value as the index of Aux[] array
+    for(int i=0; i<N; i++) {
+        Aux[A[i]]++;
+    }
+
+    int j = 0;
+    for(int i=0; i<=K; i++) {
+        int tmp = Aux[i];
+        // Aux stores which element occurs how many times,
+        // Add i in sortedA[] according to the number of times i occured in A[]
+        while(tmp--) {
+            //cout << Aux[i] << endl;
+            sortedA[j] = i;
+            j++;
+        }
+    }
+}
+int main()
+{
+	int N;
+	cin>>N;
+	int A[N];
+	for(int i=0;i<N;i++)
+		cin>>A[i];
+	int sortedA[N];
+	counting_sort(A,sortedA,N);
+	for(int i=0;i<N;i++)
+		cout<<sortedA[i]<<" ";
+	return 0;
+}
