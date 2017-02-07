0f0206cd570b7b519b5701eaaa9fc264e75b044d
Ashutosh Maurya <ashutoshmauryabgp@gmail.com>
2017-02-07 20:14:30 +0530

    create kadane's implementation in python

---
+++ Maximum Sub-Array Problem/kadane_python.py
@@ -0,0 +1,6 @@
+def max_subarray(A):
+    max_ending_here = max_so_far = A[0]
+    for x in A[1:]:
+        max_ending_here = max(x, max_ending_here + x)
+        max_so_far = max(max_so_far, max_ending_here)
+    return max_so_far
