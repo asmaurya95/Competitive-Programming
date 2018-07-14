b56838b8e182935a544ce5cd6b0418ae12b57451
asmaurya95 <ashutoshmauryabgp@gmail.com>
2018-07-14 19:34:43 +0530

    removed errors from fibonacci series using dp

--- Dynamic-Programming/Fibonacci-Series.cpp
+++ Dynamic-Programming/Fibonacci-Series.cpp
@@ -1,21 +1,20 @@
-//This is a C++ Program to print Fibonacci Series upto a given number entered by the user
-//solved using basic Dynamic Programming Technique
 #include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
   vector <long int> fib;
   long int n;
   cout<<"Enter the length of fibonacci series to print :";
   cin>>n;
-  fib[0]=fib.push_back(0);
-  fib[1]=fib.push_back(1);
+  fib.push_back(0);
+  fib.push_back(1);
   for(long int i=2;i<=n;i++)
   {
-    fib[i]=fib.push_back(fib[i-1]+fib[i-2]); //Here I am using the values stored in the past, reducing the time at cost of more space (Compared to Recursive Solution)
+    fib.push_back(fib[i-1]+fib[i-2]); //Here I am using the values stored in the past, reducing the time at cost of more space (Compared to top down Recursive Solution)
   }
   //Printing the Series
-  for(long int i=0;i<fib.size();fib++)
+  for(long int i=0;i<fib.size();i++)
   {
     cout<<fib[i]<<", ";
   }
--- Sphere Online Judge/FCTRL/README.md
+++
@@ -1,3 +0,0 @@
-# Counting the number of trailing zeroes in a Factorial
-
-Visit this link : https://brilliant.org/wiki/trailing-number-of-zeros/ for reference.
